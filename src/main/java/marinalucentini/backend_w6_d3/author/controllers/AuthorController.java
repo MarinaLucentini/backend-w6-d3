e3aee6cdead057765df4d0d0e646466defc9913b
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-06-26 15:15:23 +0200

    get and pagination

--- src/main/java/marinalucentini/backend_w6_d3/author/controllers/AuthorController.java
+++ src/main/java/marinalucentini/backend_w6_d3/author/controllers/AuthorController.java
@@ -3,6 +3,7 @@ package marinalucentini.backend_w6_d3.author.controllers;
 import marinalucentini.backend_w6_d3.author.entities.Author;
 import marinalucentini.backend_w6_d3.author.services.AuthorServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +16,10 @@ public class AuthorController {
     @ResponseStatus(HttpStatus.CREATED)
     public Author saveAuthor(@RequestBody Author newAuthor){
         return authorServices.saveAuthor(newAuthor);
+    }
+    @GetMapping
+    public Page<Author> getAllUsers(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "5") int size, @RequestParam(defaultValue = "name") String sortBy) {
+        return authorServices.getAuthors(page, size, sortBy);
     }
 
 }
--- src/main/java/marinalucentini/backend_w6_d3/author/services/AuthorServices.java
+++ src/main/java/marinalucentini/backend_w6_d3/author/services/AuthorServices.java
@@ -5,6 +5,10 @@ import marinalucentini.backend_w6_d3.author.exceptions.BadRequestException;
 import marinalucentini.backend_w6_d3.author.repository.AuthorRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -13,6 +17,7 @@ import java.time.LocalDate;
 public class AuthorServices {
     @Autowired
     AuthorRepository authorRepository;
+    // salvato l'utente correttamente nel db dopo aver fatto il post
     public Author saveAuthor(Author newAuthor){
 authorRepository.findByEmail(newAuthor.getEmail()).ifPresent(
         author -> {
@@ -20,7 +25,14 @@ authorRepository.findByEmail(newAuthor.getEmail()).ifPresent(
         }
 );
 newAuthor.setDateOfBirth(LocalDate.now().minusYears(22));
-        System.out.println("L'autore è stato correttamente salvato nel db" + newAuthor.getName() + newAuthor.getDateOfBirth());
+        System.out.println("L'autore " + newAuthor.getName() + " è stato correttamente salvato nel db." );
       return   authorRepository.save(newAuthor);
     }
+    // paginazione
+    public Page<Author> getAuthors(int pageNumber, int pageSize, String sortBy){
+        if(pageSize> 50) pageSize = 50;
+        Pageable pageable= PageRequest.of(pageNumber, pageSize, Sort.by(sortBy));
+        return authorRepository.findAll(pageable);
+    }
+
 }
